c87b60b49f81d56d5f3238529b50e529db9b4c06
andrewp-li <80422758+andrewp-li@users.noreply.github.com>
2023-05-09 06:05:36 -0400

    report, figures, tex

--- assignment_3/functions.py
+++ assignment_3/functions.py
@@ -1,27 +1,38 @@
 import numpy as np
 
-def f(z0,c,epsilon,max_iter):
+def f(z0,c,max_iter):
+    '''Iterate z_i+1=z^2+c until divergent
+    
+    Paramters:
+    z0 - A complex number. The initial guess
+    c - A complex number. The value of c to use
+    max_iter - An integer. The maximum iterations
+    
+    Returns:
+    An integer. The number of iterations to diverge, or np.nan if convergent.
+    '''
     for i in range(max_iter):
         z1 = z0**2 + c #iterate
-        if np.abs(z1) > 2: #met criteria for divergence
+        if np.abs(z1) > 2: # criteria for divergence, return the iteration
             return i
         else: #update
             z0 = z1
     return np.nan #converges
 
-def complex_iterate(N=100,z0=0,epsilon=1e-4,max_iter=1000):
-    '''Iterates method of findign the roots of a function.
+def complex_iterate(N=100,z0=0,max_iter=1000):
+    '''Applies f to a complex plane
     
     Paramters:
     N - An integer. Creates NxN grid to sample points
-    z0 - A float. The initial guess
-    epsilon - A float. The tolerance
+    z0 - A complex number. The initial guess
     max_iter - An integer. The maximum iterations
     
     Returns:
-    2d numpy array. The coordinates of each is the number of iterations to converge, np.nan if divergent.
+    2d numpy array. The coordinates of each is the number of iterations to diverge, np.nan if convergent.
     '''
+    # Creating complex plane
     arr = np.linspace(-2,2,N)
     re, im = np.meshgrid(arr,arr*1j)
     c = re + im
-    return [[f(z0,c[i][j],epsilon,max_iter) for j in range(N)] for i in range(N)]
+    
+    return [[f(z0,c[i][j],max_iter) for j in range(N)] for i in range(N)] # runs f for each point in the complex plane
